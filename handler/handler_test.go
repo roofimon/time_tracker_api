e7ae572f0843ad65652bf58839b9ad3d155dedda
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2014-08-24 11:15:52 +0700

    refactor test  status code to function

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -56,10 +56,6 @@ func TestCheckinHandler(t *testing.T) {
 
 	CheckinHandler(recorder, request)
 
-	if recorder.Code != http.StatusCreated {
-		t.Errorf("expect %d but was %d", http.StatusCreated, recorder.Code)
-	}
-
 	var (
 		expectedJSON = `{"id":"53f87e7ad18a68e0a884d31e"}`
 		actualJSON   = recorder.Body.String()
@@ -68,6 +64,7 @@ func TestCheckinHandler(t *testing.T) {
 		t.Errorf("expected %s but was %s", expectedJSON, actualJSON)
 	}
 
+	testStatusCode(t, recorder, http.StatusCreated)
 	testContentTypeJSON(t, recorder)
 }
 
@@ -80,15 +77,18 @@ func TestCheckoutHandler(t *testing.T) {
 
 	CheckoutHandler(recorder, request)
 
-	if recorder.Code != http.StatusAccepted {
-		t.Errorf("expect %d but was %d", http.StatusCreated, recorder.Code)
-	}
-
+	testStatusCode(t, recorder, http.StatusAccepted)
 	testContentTypeJSON(t, recorder)
 }
 
 func testContentTypeJSON(t *testing.T, recorder *httptest.ResponseRecorder) {
 	if recorder.Header().Get("Content-Type") != "application/json" {
 		t.Errorf("expected application/json but was %s", recorder.Header().Get("Content-Type"))
+	}
+}
+
+func testStatusCode(t *testing.T, recorder *httptest.ResponseRecorder, code int) {
+	if recorder.Code != code {
+		t.Errorf("expect %d but was %d", code, recorder.Code)
 	}
 }
